cc473f83b9eb77d470179fd8c936d38a63443df2
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-01-14 15:28:01 +0900

    render TestItem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import SortableList from "./lib/SortableList";
+import { data } from "./TestItem/TestData";
+import TestItem from "./TestItem/TestItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const onClickItem = (index) => alert(index);
+  const onDropItem = (newList) => console.log(newList);
 
+  return (
+    <SortableList
+      data={data}
+      renderItem={(item, index) => <TestItem item={item} index={index} />}
+      onDropItem={onDropItem}
+      onClickItem={onClickItem}
+    />)
+
+}
 export default App;
